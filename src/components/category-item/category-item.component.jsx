d673332a44fe300b7a9bf31ea8d34f5592229fd9
amirulkanak <coder.kanak@gmail.com>
2024-01-28 00:17:11 +0600

    Directory page with category components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <img className="background-image" src={imageUrl} alt={title} />
+      <div className="category-body-container">
+        <h2 className="category-title">{title}</h2>
+        <p className="shop-btn">Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
